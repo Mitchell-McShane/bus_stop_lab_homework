2ca44885f0023fdaf48f40f23c9d0d8b3f8bfbd1
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2020-11-10 16:14:07 +0000

    step 1

--- src/bus.py
+++ src/bus.py
@@ -1,3 +1,9 @@
 class Bus:
-    def __init__(self):   
-        pass
+    def __init__(self, route_number, destination,):
+        self.route_number = 22
+        self.destination = destination
+        self.passengers = []
+
+    def drive(self):
+        return "Brum brum"
+        
